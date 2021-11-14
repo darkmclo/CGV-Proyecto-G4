08e193100a24eded72ddf557613665cf360101cf
Daniel Carmona <daniellamarash2000@gmail.com>
2021-11-14 17:12:02 -0600

    VIDA Y DAÑO AL PERSONAJE

---
+++ CGV-Game/Assets/Scrips/MakeDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeDamage : MonoBehaviour
+{
+    public int cantidad = 10;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Untagged")
+        {
+            other.GetComponent<Health_and_Damage>().RestarVida(cantidad);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Untagged")
+        {
+            other.GetComponent<Health_and_Damage>().RestarVida(cantidad);
+        }
+    }
+}
